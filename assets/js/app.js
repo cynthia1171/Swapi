67e54bbd1d66fb9ce1ba9ffb7d6d8c2b37c095b0
cynthia1171 <c.isla1171@gmail.com>
2018-02-15 19:02:04 -0300

    aplicando estilos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $('#myModal').modal('toggle')
 
-  var id = 0;
-  $.ajax({
-    url: 'https://swapi.co/api/people',
-    type: 'GET',
-  }).done(function (data) {
-    var arr = data.results;
-    for (var i in arr) {
-      var datos = arr[i];
-      $('.monitos').append('<div class="col-md-4 cardPer" id="' + i +'" data-toggle="modal" data-target="#modal">' + datos.name + '</div>' +
-        '');
-    }
+  $('.cardPer').click(function () {
+    var id = $(this).attr('id');
 
-    $('.cardPer').click(function () {
-      var valor = parseInt($(this).attr('id'));
-      id = valor +1;
-      alert(id);
+    $.ajax({
+      url: 'https://swapi.co/api/people/' + id,
+      type: 'GET',
+    }).done(function (data) {
+      console.log(data);
     })
+      .fail(handleError);
   })
-    .fail(handleError);
 });
 
 
 /* funcion que se ejecuta cuando ocurre algún error */
 function handleError() {
-  console.log('Se ha presentado un error');
+  console.log('Se ha producido un error');
 }
+
+
 
