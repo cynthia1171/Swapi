379ea1e534f0056d9d7652d6896109967eec0a00
cynthia1171 <c.isla1171@gmail.com>
2018-02-15 20:51:24 -0300

    aplicando estilo de google a js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
-$(document).ready(function () {
+$(document).ready(function() {
   /* llama a modal */
-  $('#myModal').modal('toggle')
+  $('#myModal').modal('toggle');
 
   /* funcion que se ejecuta al presionar cada imagen del personaje de star wars */
-  $('.cardPer').click(function () {
+  $('.cardPer').click(function() {
     /* se vacian los input */
-    $('.modal-title').empty()
+    $('.modal-title').empty();
     $('.modal-body').empty();
 
     var id = $(this).attr('id');/* se obtiene el id del div, que es el que se utiliza para acceder a la información del personaje */
@@ -14,47 +14,46 @@ $(document).ready(function () {
     $.ajax({
       url: 'https://swapi.co/api/people/' + id,
       type: 'GET',
-    }).done(function (data) {
+    }).done(function(data) {
       $('.modal-title').text(data.name);/* se rellena con el nombre */
       
       /* el body del modal se rellena con la información del personaje que es requerida */
-      $('.modal-body').append('<div class="form-group">'+
-                                '<label for="inputHeight">Estatura</label>'+
-                                '<input type="text" class="form-control" id="inputHeight" value="'+data.height+'">'+
-                              '</div>'+
+      $('.modal-body').append('<div class="form-group">' +
+                                '<label for="inputHeight">Estatura</label>' +
+                                '<input type="text" class="form-control" id="inputHeight" value="' + data.height + '">' +
+                              '</div>' +
                               '<div class="form-group">' +
                                 '<label for="inputMass">Peso</label>' +
-                                '<input type="text" class="form-control" id="inputMass" value="'+data.mass+'">' +
+                                '<input type="text" class="form-control" id="inputMass" value="' + data.mass + '">' +
                               '</div>' +
                               '<div class="form-group">' +
                                 '<label for="inputHair">Color de pelo</label>' +
-                                '<input type="text" class="form-control" id="inputHair" value="'+data.hair_color+'">' +
+                                '<input type="text" class="form-control" id="inputHair" value="' + data.hair_color + '">' +
                               '</div>' +
                               '<div class="form-group">' +
                                 '<label for="inputEye">Color de ojos</label>' +
-                                '<input type="text" class="form-control" id="inputEye" value="'+data.eye_color+'">' +
+                                '<input type="text" class="form-control" id="inputEye" value="' + data.eye_color + '">' +
                               '</div>' +
                               '<div class="form-group">' +
                                 '<label for="inputBirth">Cumpleaños</label>' +
-                                '<input type="text" class="form-control" id="inputBirth" value="'+data.birth_year+'">' +
+                                '<input type="text" class="form-control" id="inputBirth" value="' + data.birth_year + '">' +
                               '</div>' +
                               '<div class="form-group">' +
                                 '<label for="inputGender">Género</label>' +
-                                '<input type="text" class="form-control" id="inputGender" value="'+data.gender+'">' +
+                                '<input type="text" class="form-control" id="inputGender" value="' + data.gender + '">' +
                               '</div>' +
                               '<div class="form-group">' +
                                 '<label for="inputSkin">Color de piel</label>' +
-                                '<input type="text" class="form-control" id="inputSkin" value="'+data.skin_color+'">' +
+                                '<input type="text" class="form-control" id="inputSkin" value="' + data.skin_color + '">' +
                               '</div>' 
       );
     })
       .fail(handleError);
-  })
+  });
 });
 /* funcion que se ejecuta cuando ocurre algún error */
 function handleError() {
   console.log('Se ha producido un error');
 }
-
 
 
